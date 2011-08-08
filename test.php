8dc955d01355f9490c40c5e71b1011104a6f3771
Keren Ben-Arosh <kerenb20@gmail.com>
2011-08-08 13:52:52 +0300

    keren

---
+++ test.php
@@ -0,0 +1,205 @@
+<?php
+	include("bin/load_config.php");
+?>
+
+
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Pop/AS Visualizer</title>
+        <!-- <script src="http://code.jquery.com/jquery-latest.js"></script> -->
+        <script src="js/jquery-1.6.2.min.js" type="text/javascript"></script>
+        <script type="text/javascript" src="js/loadData.js"></script>
+          
+          
+          <script type="text/javascript">
+			 // Test connection to blade ---------------------------------------------------          
+             function testConnection() {             	                
+                $.post("query_backend.php", { func: "testConnection", blade: $("#mySelect").val() },
+                    function(data){
+                             var result = data.result;
+                             if (data.type =="ERROR")
+                             	{alert(result);}                             
+                    }, "json");	
+             }
+             
+            $(document).ready(function() {                   
+                    $("#mySelect").change(function() {
+                    	if ($("#mySelect").val() != "Selecte blade"){
+                       		testConnection();
+                       	}
+                    });
+            });
+            
+            //get year by blade ---------------------------------------------------------------
+              $(document).ready(function() {
+                    $("#mySelect").change(function() {                                   
+                        $.post("query_backend_keren.php", {func: "showYear", blade: "B4" },
+                        //$.post("query_backend_keren.php", {func: "showWeek", blade: $("#mySelect").val()},
+                        function(data){                        			
+	                         var allYears = data.result;
+	                         var years = allYears.split(" ");
+	                         
+	                         for(i = 0; i < years.length; i++){								
+								 $("#year").append("<option>" + years[i] + "</option>");								 
+							 }							 
+                        }, "json");	
+                    });
+            });
+            
+            //get week by year ----------------------------------------------------------------------
+              $(document).ready(function() {
+                    $("#year").change(function() {                                   
+                        $.post("query_backend_keren.php", {func: "showWeek", blade: "B4" , year: $("year").val()},
+                        //$.post("query_backend_keren.php", {func: "showWeek", blade: $("#mySelect").val() , year: $("year").val()},
+                        function(data){                        			
+	                         var allWeeks = data.result;
+	                         var weeks = allWeeks.split(" ");
+	                         
+	                         for(i = 0; i < weeks.length; i++){								
+								$("#week").append("<option>" + weeks[i] + "</option> "); 
+							 }
+	                         
+                        }, "json");	
+                    });
+            });
+            
+            // get all relevant AS by parameters
+              $(document).ready(function() {
+                    $("#getAS").click(function() {                                                           
+                        $.post("query_backend_keren.php", {func: "getASlist", blade: $("#mySelect").val() , year: $("year").val() , week: $("week").val()},
+                        function(data){                        			
+	                         var allAS = data.result;
+	                         var AS = allAS.split(" ");
+	                         
+	                         for(i = 0; i < AS.length; i++){								
+								$("#ASlist").append("<option>" + AS[i] + "</option> "); 
+							 }
+	                         
+                        }, "json");	
+                    });
+            });            
+            </script>
+            
+            <style type="text/css">
+            	table{margin-left:auto; margin-right:auto;}
+				table.imagetable {
+				font-family: verdana,arial,sans-serif;
+				font-size:11px;
+				color:#333333;
+				border-width: 1px;
+				border-color: #999999;
+				border-collapse: collapse;
+			}
+			table.imagetable th {
+				background:#b5cfd2 url('images/table-images/cell-blue.jpg');
+				border-width: 1px;
+				padding: 8px;
+				border-style: solid;
+				border-color: #999999;
+			}
+			table.imagetable td {
+				background:#dcddc0 url('images/table-images/cell-grey.jpg');
+				border-width: 1px;
+				padding: 8px;
+				border-style: solid;
+				border-color: #999999;
+			}
+			</style>
+
+                  
+    </head>
+
+    
+    
+    <body>        
+        
+        <div id="container" style="font-family:font-family: verdana,arial,sans-serif;">
+        	<!--comic sans ms;-->
+
+            <div id="header">
+                <h1 style="margin-bottom:10px;text-align:center;color:Navy">PoP/AS Visualizer</h1>
+                <h3 style="text-align: left; margin-left: 5px">Welcome, <?php echo $_SESSION['username']; ?></h3>
+            </div>
+						                       
+            <div id="user-select" style="margin-left:3%;background-color:#FFD700;color:#333333;width:27%;
+            height: 85%; float:left;clear: none">
+            <h3 style="text-decoration: underline;text-align: center">Make a new query</h3>
+                
+                <form style="font-size:14px;">
+                	<h4 style="color:teal; margin-bottom: 10px; font-size:16px;">Select blade</h4>
+                    <!-- <legend style="color:teal">Choose blade:</legend> -->
+                    Blade:
+                    <select id="mySelect">
+                    	<option value="">Select blade</option>
+                            <?php
+                            //echo '<option>Select blade</option>';                              
+                            foreach($Blades as $blade)
+                                    {
+                                        $name = $blade["@attributes"]["name"];
+										var_dump($name);
+                                        if($name!="" && $Blade_Map[$name]["db"]=="DIMES_DISTANCES")
+                                                echo "<option>$name</option>";
+                                    }
+                            ?>
+                    </select>          
+                </form>                               
+                
+                <form id="AS" name="get AS list" style="font-size:14px;">                               
+                    <h4 style="color:teal; margin-bottom: 10px; font-size:16px;">Select table</h4>
+       
+                    <div align="left">Year :                       
+                        <select id="year" >
+                            <option value="">Select year</option>                            
+                        </select>
+                    </div>
+
+                     <div align="left">Week:
+                        <select id="week">                               
+                            <option selected="selected" value="">Select week</option>                            
+                        </select>
+                    </div>                        
+               
+               		
+                    <input id="getAS" type="submit" value="Get AS list!" style="margin-left: 20px; margin-top: 10px"/>
+                    
+                    <div id="ASlist">
+                    	
+                    	
+                    </div>    
+                    
+                </form>
+              
+            </div>
+            
+            <div id="My_queries" style="margin-right: 3%;background-color:#EEEFEE;width:67%;height:85%;float: right; clear:right; text-align:center">
+                <h3>My queries</h3>                	
+                <br></br>                
+
+				<table class="imagetable" style="alignment-baseline: central">
+				<tr>
+					<th>Query ID</th><th>SQL query</th><th>Status</th><th>Abort</th>
+				</tr>								
+				<tr>
+					<td>123</td><td>Text 1B</td><td>Text 1C</td><td>Text 1D</td>
+				</tr>
+				<tr>
+					<td>765</td><td>Text 2B</td><td>Text 2C</td><td>Text 2D</td>
+				</tr>
+				</table>
+
+                
+                
+                
+                
+                
+                
+            </div>
+            
+            <div id="footer" style="background-color:#FFA500;clear:both;text-align:center;margin-top: 10px">
+                Copyright © 2011 DIMES
+            </div>
+            
+         </div>
+    </body>
+</html>
