c68387bfb60d7196ebf8439655f3e864500c86dc
Keren Ben-Arosh <kerenb20@gmail.com>
2011-08-11 23:29:20 +0300

    writing to xml file - done.

--- writeToXML.php
+++ writeToXML.php
@@ -2,37 +2,21 @@
  
 function AddQuery($queryID,$processID,$usertoadd,$EdgeTbl, $PopTbl) 
 {
-	$nameXML = "xml/query.xml";	 
+	 $nameXML = "xml/query.xml";	 
 	// Load XML file
-	$xml = simplexml_load_file($nameXML);
-	echo "loading xml file";
+	 $xml = simplexml_load_file($nameXML);
+	
+	 $query = $xml->addChild('QUERY');
+	 $query->addChild('queryID', $queryID);
+	 $query->addChild('processID', $processID);
+	 $query->addChild('lastKnownStatus',"running");
+	 $query->addChild('EdgeTbl', $EdgeTbl);
+	 $query->addChild('PopTbl', $PopTbl);
+	 $users = $query->addChild('users');
+	 $users->addChild('user', $usertoadd);
 	 
-	// Find all instances of <album>
-	 $seg = $xml->DATA;
-	 //foreach($xml->DATA as $seg)
-	 {
-	 		echo "trying to add query";
-		// Find album title you want to add to
-		 // if($seg['title'] == $fgallery) 
-		  {
-		 
-		// Create QUERY element
-		   //$QUERY = $seg->addChild('QUERY');
-		 
-		// Add attributes to element
-		   $QUERY->addAttribute('queryID', $queryID);
-		   $QUERY->addAttribute('processID', $processID);
-		   $QUERY->addAttribute('usertoadd', $usertoadd);
-		   $QUERY->addAttribute('EdgeTbl', $EdgeTbl);
-		   $QUERY->addAttribute('PopTbl', $PopTbl);
-		 
-		// Save XML file
-		   $xml->asXML($nameXML);
-		}
-	}
+	 $xml->asXML($nameXML);
+
 } 
- 
-AddQuery("1111", "2222", "keren", "edge1", "pop1");
- 
 
 ?>
