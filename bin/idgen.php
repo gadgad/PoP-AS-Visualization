2bfa3b2434648e3cce723fcde872dd9ce9290fed
Gadi Sirota <gadi.sirota@gmail.com>
2011-08-11 20:01:34 +0300

    new stuff

--- bin/idgen.php
+++ bin/idgen.php
@@ -6,12 +6,17 @@
     	private $asList;
     	private $queryID;
     	
-    	public function __construct($edgeTbl,$popTbl,$asList)
+    	public function __construct()
     	{
-    		$this->edgeTbl = $edgeTbl;
-    		$this->popTbl = $popTbl;
-    		$this->asList = $asList;
-    		$this->queryID = md5($edgeTbl."_".$popTbl.join('_',$asList));
+    		if(func_num_args()==3)
+    		{
+    			$this->edgeTbl = func_get_arg(0);
+    			$this->popTbl = func_get_arg(1);
+    			$this->asList = func_get_arg(2);
+    			$this->queryID = md5($this->edgeTbl."_".$this->popTbl.join('_',$this->asList));
+			} else if(func_num_args()==1) {
+				$this->queryID = func_get_arg(0);
+			}
     	}
     	
     	public function getEdgeTblName() { return "DPV_EDGE_".$this->queryID; }
