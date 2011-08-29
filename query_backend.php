0415477a74e91bc4a830f18b8e75aa0149fc1ac4
Gadi Sirota <gadi.sirota@gmail.com>
2011-08-29 17:20:51 +0300

    improvement in query status identification

--- query_backend.php
+++ query_backend.php
@@ -3,6 +3,7 @@
 	include_once("bin/idgen.php");
 	include_once("writeToXML.php");
 	include_once("bin/win_backgrounder.php");
+	require_once("bin/query_status.php");	
 				
 	// Turn off all error reporting
 	error_reporting(0);
@@ -277,50 +278,10 @@
 		// check if present in 'SHOW PROCESSLIST' and/or table exist, if so add to query.xml 
 		if($stage==3)
 		{
-			session_start();
-			$pid1 = isset($_SESSION['pid1'])?$_SESSION['pid1']:'null';
-			$pid2 = isset($_SESSION['pid2'])?$_SESSION['pid2']:'null';
 			
-			// 0 - error , 1 - running , 2 - complete
-			$edge_state = 0;
-			$pop_state = 0;
+			$query_status = getQueryStatus($queryID);
 			
-			$mysqli = new mysqli($host,$user,$pass,$database,$port);
-					
-			while($mysqli->connect_error) {
-					sleep(3);
-					//$mysqli->ping();
-					$mysqli = new mysqli($host,$user,$pass,$database,$port);
-	 		    	//ret_res('Connect Error (' . $mysqli->connect_errno . ') '. $mysqli->connect_error,"ERROR");
-			}
-			
-			$result = $mysqli->query('SHOW FULL PROCESSLIST;');
-			$num = $result->num_rows;
-			for($x = 0 ; $x < $num ; $x++){
-			    $row = $result->fetch_assoc();
-			    if($row['State']!=NULL){
-			    	//echo $row['Id']." ".$row['Info']."\n";
-			    	$tbl = strstr(strstr( $row['Info'] ,'DPV_'),'`',true);
-					if($idg->getPoPTblName()==$tbl){
-						$pop_state = 1;
-					} else if($idg->getEdgeTblName()==$tbl){
-						$edge_state = 1;
-					}
-			    }
-			}
-			if($pop_state==0){
-				$result = $mysqli->query("show tables from DIMES_POPS_VISUAL like '".$idg->getPoPTblName()."'");
-				$num = $result->num_rows;
-				if($num>0) $pop_state = 2;	
-			}
-			if($edge_state==0){
-				$result = $mysqli->query("show tables from DIMES_POPS_VISUAL like '".$idg->getEdgeTblName()."'");
-				$num = $result->num_rows;
-				if($num>0) $edge_state = 2;	
-			}
-			$mysqli->close();
-			
-			if($pop_state==0 || $edge_state==0){
+			if($query_status == 0){
 				//ret_res("sql query failed to execute properly...</BR>try running the following queries directly:</BR>".(($pop_state==0)?$query1:"")."</BR>".(($edge_state==0)?$query2:""),"ERROR");
 				ret_res("sql query failed to execute properly...</BR>","ERROR");
 			} else {
@@ -334,7 +295,7 @@
 					}
 				}
 				header('Content-type: application/json');
-				echo json_encode(array("result"=>"query is now running..." ,"type"=>"GOOD","queryID"=>$queryID,"pid1"=>$pid1,"pid2"=>$pid2));
+				echo json_encode(array("result"=>"query is now running..." ,"type"=>"GOOD","queryID"=>$queryID));
 			}
 		}	
 	}
