7769ea985514d7dad3f1ca0db68bf8425eb3ecfc
Gadi Sirota <gadi.sirota@gmail.com>
2011-08-29 15:40:16 +0300

    gadi

--- user_query_managment.php
+++ user_query_managment.php
@@ -1,9 +1,18 @@
 <?php
-	include_once("bin/load_config.php");	
+	require_once("bin/load_config.php");
+	require_once("bin/idgen.php");	
+	
+	if(!isset($_REQUEST["query"]) || !isset($_REQUEST["func"]))
+		ret_res('missing parameters!','ERROR');
+	
 	
 	// globals
-	$queryID = $_POST["query"];
-	$username = $_POST["username"];
+	$queryID = $_REQUEST["query"];
+	if(isset($_REQUEST["username"])){
+		$username = $_REQUEST["username"];
+	} else {
+		$username = isset($_COOKIE['username'])? $_COOKIE['username'] : $_SESSION['username'];
+	}
 		
 	$queries = simplexml_load_file("xml\query.xml");
 	$res = $queries->xpath('/DATA/QUERY[queryID="'.$queryID.'"]/blade');
@@ -14,7 +23,8 @@
 	$hostNport = (string)$blade["host"].":".(string)$blade["port"];
 	$user = (string)$blade["user"];
 	$pass = is_array($blade["pass"])?"":(string)$blade["pass"];
-	$database = (string)$blade["db"];		
+	$database = (string)$blade["db"];
+	
 	
 	function ret_res($message, $type)
 	{
@@ -66,9 +76,117 @@
 	     reset($objects); 
 	     rmdir($dir); 
 	   } 
-	 } 
+	 }
+	  
+	
+	if($_REQUEST["func"]=="getRunningStatus")
+	{
 		
-	if($_POST["func"]=="abort")
+		$queries = simplexml_load_file("xml\query.xml");									
+		$result = $queries->xpath('/DATA/QUERY[queryID="'.$queryID.'"]');		
+		
+		if($result==FALSE){ // the query is not found in the queries file
+			ret_res('Assertion Error - query is not found in query list','ERROR');			
+		}
+		
+		/*
+		if ($result[0]->lastKnownStatus!="running"){
+			ret_res('Assertion Error - this method should only be called for running processes','ERROR');
+		}
+		 * 
+		 */
+
+		$idg = new idGen($queryID);
+		$kml_dst_dir = 'queries/'.$idg->getDirName();
+		$kml_filename = $kml_dst_dir.'/result.kmz';
+		$edges_filename = $kml_dst_dir.'/edges.xml';
+		$pop_filename = $kml_dst_dir.'/pop.xml';
+		
+		if(file_exists($kml_filename)){
+			ret_res('kml file is ready','COMPLETE');
+		}
+		
+		if(file_exists($edges_filename) || file_exists($pop_filename)){
+			ret_res('fetchnig data into xml files and/or rendering kml','RUNNING');
+		}
+	
+		// xml/kml files are not present...cheking status on mysql server
+		// 0 - error , 1 - running , 2 - tables ready
+		$edge_state = 0;
+		$pop_state = 0;
+		
+		$mysqli = new mysqli($host,$user,$pass,$database,$port);
+				
+		while($mysqli->connect_error) {
+				sleep(3);
+				$mysqli = new mysqli($host,$user,$pass,$database,$port);
+ 		    	//ret_res('Connect Error (' . $mysqli->connect_errno . ') '. $mysqli->connect_error,"ERROR");
+		}
+		
+		$result = $mysqli->query('SHOW FULL PROCESSLIST;');
+		$num = $result->num_rows;
+		for($x = 0 ; $x < $num ; $x++){
+		    $row = $result->fetch_assoc();
+		    if($row['State']!=NULL && stristr($row['Info'],'create table')!=FALSE){
+		    	$tbl = strstr(strstr( $row['Info'] ,'DPV_'),'`',true);
+				if($idg->getPoPTblName()==$tbl){
+					$pop_state = 1;
+				} else if($idg->getEdgeTblName()==$tbl){
+					$edge_state = 1;
+				}
+		    }
+		}
+		
+		if($pop_state==1 || $edge_state==1){
+			ret_res('queries are still running..','RUNNING');
+		}
+		
+		if($pop_state==0){
+			$result = $mysqli->query("show tables from DIMES_POPS_VISUAL like '".$idg->getPoPTblName()."'");
+			$num = $result->num_rows;
+			if($num>0){
+				$result = $mysqli->query("show open tables from DIMES_POPS_VISUAL like '".$idg->getPoPTblName()."'");
+				$num = $result->num_rows;
+				if($num>0){
+					$row = $result->fetch_assoc();
+					if(intval($row['In_use'])==0){
+						$pop_state = 2;
+					}	
+				} else { // table is not open / locked
+					$pop_state = 2;
+				}
+			} 	
+		}
+		
+		if($edge_state==0){
+			$result = $mysqli->query("show tables from DIMES_POPS_VISUAL like '".$idg->getEdgeTblName()."'");
+			$num = $result->num_rows;
+			if($num>0){
+				$result = $mysqli->query("show open tables from DIMES_POPS_VISUAL like '".$idg->getEdgeTblName()."'");
+				$num = $result->num_rows;
+				if($num>0){
+					$row = $result->fetch_assoc();
+					if(intval($row['In_use'])==0){
+						$edge_state = 2;
+					}	
+				} else { // table is not open / locked
+					$edge_state = 2;
+				}
+			} 	
+		}
+		
+		$mysqli->close();
+		
+		if($pop_state==0 || $edge_state==0){
+			ret_res("query is not running and table doesnt exsist or is locked","ERROR");
+		} else if($pop_state==2 && $edge_state==2){
+			ret_res("ready to fetch data from db","RUNNING");
+		} else {
+			ret_res("assertion error - ambiguous status","ERROR");
+		}
+	}
+		
+	if($_REQUEST["func"]=="abort")
 	{
 		
 		$queries = simplexml_load_file("xml\query.xml");									
