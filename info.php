e612eef65bca8ec2c5c828c74c03ae001306a2dc
Keren Ben-Arosh <kerenb20@gmail.com>
2011-10-15 16:31:05 +0200

    info php is ready..

--- info.php
+++ info.php
@@ -3,7 +3,7 @@
     <p style="line-height:150%;padding-left: 30px;padding-right: 30px;">The POP-AS Visualizer was created to visualize geographic data and present it in an intuitive way. With the system you can query the DIMES database for POP-AS info and view the results with the Google earth plugin.</p>
 	<p style="line-height:150%;padding-left: 30px;padding-right: 30px;">
 	  <?php
-	    $filename = "User_Guide.pdf"; 
+	    $filename = "User Guide.pdf"; 
         if(file_exists($filename)) {
           echo ("for more information download the <a href=\"$filename\">user guide</a>");
         } else {
@@ -11,5 +11,9 @@
         }
     ?>    	
 	</p>             	            
-    <p>An example of a result file:</p>
+    <p>An example of the result file:</p>
+    <div align="center">
+    	<img src="images/earth_example.jpg">	
+    </div>
+    
 </div>
