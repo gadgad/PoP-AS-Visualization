fc4fe632fb2bb9cb929059576597a9aa6316e004
Keren Ben-Arosh <kerenb20@gmail.com>
2011-10-26 14:04:37 +0200

    changes to webpage

--- doc/webPage.php
+++ doc/webPage.php
@@ -1,9 +1,26 @@
 <html>
     <head>
+        
+        <style type="text/css">
+        	p{
+        		margin-left: 50 px;
+        		font-family: Arial;
+        	}
+        </style>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title>PoP/AS Visualization</title>        
         <script src="../js/jquery-1.6.2.min.js" type="text/javascript"></script>
-        <link rel="stylesheet" href="../css/visual.css" type="text/css" media="screen" />          
+        <link rel="stylesheet" href="../css/visual.css" type="text/css" media="screen" />
+        <style type="text/css">
+        	p{
+        		margin-left: 50 px;
+        		font-family: Arial;
+        		line-height: 150%;        		
+        	}
+        	h3{
+        		color: rgb(54,95,145);
+        	}
+        </style>          
     </head>
 
     <body>        
@@ -19,6 +36,24 @@
             <div id="information" style="width: 100%; margin: 30px">
                 		
 				<h1 class="login-header">Info</h1>
+				<p>The POP/AS visualization system is a visual, easy-to-use interface to the NetDimes database, providing PoP-level PoP-AS geographic information. </p>
+				<p>It allows querying the DB and viewing the results on an interactive map.</p>
+				<h3>Personalized user page</h3>
+				<div style = "width: 100%;">
+					
+					<div style = "width: 30%; float: left; height: 60%">
+						<p>The user can generate new queries with the menu on the left.</p>
+						<p>On the right the user can view his previous queries and details.</p>
+						<br />
+					</div>
+					<div style = "width: 70%; float: right; height: 60%"> <img src="../images/index.png" /> </div>
+					
+				</div>
+				<h3>Visualization examples</h3>
+				<div align="center"> <img src="../images/week42.png" /> </div>
+				<br />
+				<br />
+				<div align="center"> <img src="../images/earth_example.jpg" /> </div>
 				
 	            		
 	            					
