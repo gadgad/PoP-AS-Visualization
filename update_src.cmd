70744166e0a4b7655ade314a07365b181aa52413
Gadi Sirota <gadi.sirota@gmail.com>
2011-11-09 02:02:32 +0200

    .

--- update_src.cmd
+++ update_src.cmd
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+sudo mv -f update_src.cmd update_src.bak
 sudo mv -f config/config.xml config/config.backup
 sudo rename .xml .local xml/*.xml 
 sudo rename .data .local data/*.data
 sudo rename .xml .local users/*.xml
+sudo rename .kmz .local `find . -name *.kmz`
 sudo git pull
 sudo mkdir -p shell/log/queries
 sudo mkdir -p mail_log
@@ -13,4 +15,8 @@ sudo mv	-f config/config.backup	config/config.xml
 sudo rename .local .xml xml/*.local
 sudo rename .local .data data/*.local
 sudo rename .local .xml users/*.local
-sudo chmod -R 777 config data queries shell users xml mail_log
+cd queries
+sudo rename .local .kmz `find . -name *.local`
+cd ..
+sudo chmod -R 777 config data queries shell users xml mail_log update_src.cmd
+
